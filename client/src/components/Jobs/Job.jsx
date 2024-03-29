013af3187c8b03d9d99b40bf26be38a85dda8489
Frank <frankkashere@gmail.com>
2024-03-29 13:49:21 +0300

    Updated with List of Jobs

--- client/src/components/Jobs/Job.jsx
+++ client/src/components/Jobs/Job.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const Job = ({ job }) => {
   return (
     <div className="job">
-      <li>
+      
+      <h3> <strong>Job ID :</strong> {job.id}</h3>
       <h3> <strong>Job Title :</strong> {job.title}</h3>
-      <p><strong>Location:</strong> {job.location}</p>
-      <p><strong>Job Description:</strong>{job.description}</p>
-      </li>
+      <h3><strong>Teaching Level:</strong> {job.location}</h3>
+      <h3><strong>Job Description:</strong>{job.description}</h3>
+      <p><strong>Academic Requirements:</strong>{job.academic_requirements}</p>
+      <br />
     </div>
   );
 }
--- client/src/components/Jobs/JobsList.jsx
+++ client/src/components/Jobs/JobsList.jsx
@@ -1,6 +1,6 @@
-
 import React from "react";
 import Job from "./Job"; // Importing the Job component
+import "./JobsList.css";
 
 const JobsComponent = () => {
   const jobs = [
@@ -9,29 +9,52 @@ const JobsComponent = () => {
       title: "Math Teacher",
       location: "High School",
       description: "Teach mathematics to high school students.",
+      academic_requirements:
+        "Applicant must me a bachelor's " +
+        "degree holder in the relevant subject. Masters degree will be an added advantage.",
     },
     {
       id: 2,
       title: "Science Teacher",
       location: "Middle School",
       description: "Teach science to middle school students.",
+      academic_requirements:
+        "Applicant must me a bachelor's " +
+        "degree holder in the relevant subject. Masters degree will be an added advantage.",
     },
     {
       id: 3,
       title: "Librarian",
       location: "Elementary School",
       description: "Manage library resources and assist students.",
+      academic_requirements:
+        "Applicant must me a bachelor's " +
+        "degree holder in the relevant subject. Masters degree will be an added advantage.",
     },
   ];
 
   return (
-    <div>
-      <h2>Current Job Openings:</h2>
-      <div className="jobs-list">
+    <div className="jobs-list">
+      <h2>
+        <strong>
+          <u>Current Job Openings</u>
+        </strong>
+      </h2>
+
+      <div>
         {jobs.map((job) => (
           <Job key={job.id} job={job} />
         ))}
       </div>
+
+      <h4>
+        To apply for any of the above jobs, please send your current CV and
+        application letter to{" "}
+        <a href="mailto:hr@csk.edu" style={{ color: "blue" }}>
+          <u>hr@csk.edu</u>
+        </a>
+      </h4>
+      <br />
     </div>
   );
 };
