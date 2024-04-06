b3c132750ddce4d0aeca8871770eb5723f484f69
Frank <frankkashere@gmail.com>
2024-04-06 16:13:35 +0300

    Updated changes

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import request, session, jsonify,make_response
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
-# from werkzeug.security import generate_password_hash
+
 
 from config import app, db, api
 from models import User, Department, Accounting, UserDepartment, Salary, Job
