2e38c6606e42940a17c8b0684484c7f362140024
Frank <frankkashere@gmail.com>
2024-04-08 19:57:48 +0300

    Amended dashboard component to restrict user access

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -6,16 +6,20 @@ import Profile from "./Profile";
 import "./dashboard.css";
 
 const Dashboard = ({ onLogout, onLogin, user }) => {
-    console.log("User in Dashboard:",{user}); // Log the user object here
-  return (
-    <div className="dashboard">
-      <Sidebar onLogout={onLogout} />
-      <div className="dashboard--content">
-        <Content />
-        <Profile onLogin = {onLogin} user={user} />
+  
+  if (user) {
+    return (
+      <div className="dashboard">
+        <Sidebar onLogout={onLogout} />
+        <div className="dashboard--content">
+          <Content />
+          <Profile onLogin={onLogin} user={user} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <h1>User not logged in. Please log in to view the Dashboard...</h1>;
+  }
 };
 
 export default Dashboard;
